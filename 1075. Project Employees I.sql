f05e1ae3a5ff39adf1cdcc60903a15fb6d0b7ac2
yashsj <yashsj@yahoo.co.in>
2025-06-24 00:20:33 -0500

    Create 1075. Project Employees I.sql

---
+++ 1075. Project Employees I.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT Project.project_id, ROUND(AVG(experience_years),2) AS average_years FROM Project LEFT JOIN Employee ON Project.employee_id=Employee.employee_id GROUP BY Project.project_id
